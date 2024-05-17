cbc9cc01f89e7692085f7c1f9b926629ceddd325
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-05-17 12:56:36 +0530

    Time: 17 ms (59.31%) | Memory: 26.5 MB (91.15%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+        // Moore's Voting algo
+        int me;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (count == 0) {
+                me = nums[i];
+                count = 1;
+            } else if (me == nums[i]) {
+                count++;
+            } else
+                count--;
+        }
+
+        // verify the element we got is majority element or not
+        int count1 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (me == nums[i])
+                count1++;
+        }
+        if (count1 > (nums.size() / 2))
+            return me;
+        else    
+        return -1;
+    }
+    
+};
