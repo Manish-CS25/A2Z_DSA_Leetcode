10c3d62d18add5af204e12a6c06ae9ffb4651a01
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-05-17 16:44:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (46.60%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -6,15 +6,17 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void preorder(TreeNode* node, vector<int>& v){
-        if(node == NULL) return ;
+    void preorder(TreeNode* node, vector<int>& v) {
+        if (node == NULL)
+            return;
         v.push_back(node->val);
-        preorder(node->left , v);
+        preorder(node->left, v);
         preorder(node->right, v);
     }
 
@@ -24,8 +26,5 @@ public:
         preorder(root, v);
 
         return v;
-
-
-
     }
 };
