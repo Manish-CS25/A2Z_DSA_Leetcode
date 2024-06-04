a0b784c47fd0094857473b060c6855629acb8094
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-06-04 23:04:11 +0530

    Time: 123 ms (56.94%) | Memory: 74.8 MB (55.63%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,40 +1,10 @@
-// class Solution {
-// public:
-//     int totalFruit(vector<int>& fruits) {
 
-//         // int max_b=2;
-//         map<int, int> mpp;
-
-//         int L, R, max_f;
-//         L = R = max_f = 0;
-//         while (R < fruits.size()) {
-
-//             mpp[fruits[R]]++;
-//             if (mpp.size() > 2) {
-//                 mpp[fruits[L]]--;
-//                 if (mpp[fruits[L]] == 0) {
-//                     mpp.erase(mpp[fruits[L]]);
-//                 }
-//                 L++;
-//             }
-
-//             if (mpp.size() <= 2) {
-
-//                 max_f = max(R - L + 1, max_f);
-//             }
-
-//             R++;
-//         }
-//         return max_f;
-
-//     }
-// };
 
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         unordered_map<int, int> mpp;
-        int L = 0, R = 0, max_f = 0, count = 0;
+        int L = 0, R = 0, max_f = 0;
 
         while (R < fruits.size()) {
             mpp[fruits[R]]++; // Increment the frequency of the rightmost fruit
@@ -51,13 +21,6 @@ public:
                 }
                 L++; // Move the left pointer to the right
             }
-
-            // Update the maximum number of fruits collected
-            // count = 0;
-            // for (auto it : mpp) {
-            //     count += it.second;
-            // }
-            // max_f = max(max_f, count);
 
             if (mpp.size() <= 2) {
 
