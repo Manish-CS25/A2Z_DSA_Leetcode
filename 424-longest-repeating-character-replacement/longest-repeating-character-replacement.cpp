212faa7e2fdfbc77ebee45f7f2cca8ba4f1faebb
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-06-05 23:31:12 +0530

    Time: 6 ms (79.41%) | Memory: 8 MB (99.46%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int maxlen = 0;
-        int maxf = 0; // maximum frequency of a single character in the current window
-        int count[26] = {0}; // frequency count of each character in the window
-        
-        for (int i = 0, j = 0; j < s.size(); ++j) {
-            maxf = max(maxf, ++count[s[j] - 'A']);
-            
-            // Current window size is (j - i + 1)
-            // Number of characters we need to change is (window size - maxf)
-            if (j - i + 1 - maxf > k) {
-                // If the number of changes needed is greater than k, we shrink the window from the left
-                --count[s[i] - 'A'];
-                ++i;
+        int L , R , maxf , maxlen ;
+        L = R = maxf = maxlen =0;
+        int hash[26]={0};
+
+        while(R<s.size()){
+
+            hash[s[R]-'A']++;
+            maxf = max(maxf , hash[s[R]-'A'] );
+
+            if(R-L+1-maxf>k){
+                hash[s[L]-'A']--;
+                L++;
             }
-            
-            // Update the maximum length of the valid window
-            maxlen = max(maxlen, j - i + 1);
+
+            maxlen = max(maxlen , R-L+1);
+            R++;
+
         }
-        
         return maxlen;
     }
 };
