48d28377676ede6b07e5534b4295557959e72527
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-06-09 16:07:39 +0530

    Time: 217 ms (5.08%) | Memory: 17 MB (45.01%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -8,31 +8,87 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
+// -------------Brute Force Approach ---------------
+
+// class Solution {
+// public:
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+//         vector<int> arr;
+//         for (int i = 0; i < lists.size(); i++) {
+//             ListNode* temp = lists[i];
+//             while (temp != NULL) {
+//                 arr.push_back(temp->val);
+//                 temp = temp->next;
+//             }
+//         }
+//         sort(arr.begin(), arr.end());
+
+//         if (arr.empty()) {
+//             return nullptr;
+//         }
+
+//         ListNode* head = new ListNode(arr[0]);
+//         ListNode* current = head;
+
+//         for (size_t i = 1; i < arr.size(); ++i) {
+//             current->next = new ListNode(arr[i]);
+//             current = current->next;
+//         }
+
+//         return head;
+//     }
+// };
+
+///----------------In place merging by merge 2 lists at a time------------------------
+
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int> arr;
-        for (int i = 0; i < lists.size(); i++) {
-            ListNode* temp = lists[i];
-            while (temp != NULL) {
-                arr.push_back(temp->val);
-                temp = temp->next;
+    ListNode* mergeTwoList(ListNode* head1, ListNode* head2) 
+    {
+        if (!head1)
+            return head2;
+        if (!head2)
+            return head1;
+        ListNode* ans = new ListNode(-1);
+        ListNode* ptr = ans;
+        while (head1 && head2) 
+        {
+            if (head1->val < head2->val) 
+            {
+                ptr->next = head1;
+                ptr = ptr->next;
+                head1 = head1->next;
+            } 
+            else 
+            {
+                ptr->next = head2;
+                ptr = ptr->next;
+                head2 = head2->next;
             }
         }
-        sort(arr.begin(), arr.end());
-
-        if (arr.empty()) {
-            return nullptr;
+        while (head1) 
+        {
+            ptr->next = head1;
+            ptr = ptr->next;
+            head1 = head1->next;
         }
-
-        ListNode* head = new ListNode(arr[0]);
-        ListNode* current = head;
-
-        for (size_t i = 1; i < arr.size(); ++i) {
-            current->next = new ListNode(arr[i]);
-            current = current->next;
+        while (head2) 
+        {
+            ptr->next = head2;
+            ptr = ptr->next;
+            head2 = head2->next;
         }
-
-        return head;
+        return ans->next;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) 
+    {
+        if (lists.size() == 0)
+            return NULL;
+        ListNode* head1 = lists[0];
+        for (int i = 1; i < lists.size(); i++) 
+            head1 = mergeTwoList(head1, lists[i]);
+        return head1;
     }
 };
