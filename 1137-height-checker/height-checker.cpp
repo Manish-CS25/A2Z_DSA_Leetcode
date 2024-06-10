fc1c53a1d7c63840342b5ca56e861fd0ed5f660a
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-06-10 14:04:37 +0530

    Time: 2 ms (48.33%) | Memory: 10.2 MB (44.04%) - LeetSync

---
+++ 1137-height-checker/height-checker.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int count = 0;
+        // for(int i=0;i<heights.size();i++){
+        //     if(i==0){
+        //         if(heights[i] > heights[i+1])
+        //         count++;
+        //     }
+        //     else if(i==heights.size()-1)
+        //     {
+        //         if(heights[i]<heights[i-1])
+        //         count++;
+        //     }
+        //     else{
+        //         if(heights[i]>heights[i+1] || heights[i]<heights[i-1] )
+        //         count++;
+        //     }
+        // }
+
+        vector<int> arr;
+        for (int i = 0; i < heights.size(); i++) {
+            arr.push_back(heights[i]);
+        }
+
+        sort(arr.begin(), arr.end());
+
+        for (int i = 0; i < heights.size(); i++) {
+            if (arr[i] != heights[i])
+                count++;
+        }
+        return count;
+    }
+};
