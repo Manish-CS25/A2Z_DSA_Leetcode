98b98907469d810169d878c934bf323dfc0067dd
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-06-13 16:47:15 +0530

    Time: 3 ms (37.45%) | Memory: 8.1 MB (44.15%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        // if (n == 0)
-        //     return 1.0;
-        // if (n == 1)
-        //     return x;
-        // double pro = x;
-        // if (n > 0) {
-        //     pro = pro * myPow(x, n - 1);
-        // }
-        // if (n < 0) {
-        //     pro = pro * myPow(x, n + 1);
-        // }
-        // if(n < 0)
-        // return 1 / pro;
-        // else return  pro;
+        if (n == 0)
+            return 1.0;
+        if (n < 1) {
+            n = abs(n);
+            x = 1 / x;
+        }
+
+        if (n % 2 == 0) {
+            return myPow(x * x, n / 2);
+        } else {
+            return x * myPow(x, n - 1);
+        }
 
         //// 2nd Approach
-        double ans = 1.0;
-        long long nn = n;
+        // double ans = 1.0;
+        // long long nn = n;
 
-        if (nn < 0)
-            nn = -1 * nn;
-        while (nn) {
-            if (nn % 2) {
-                ans = ans * x;
-                nn = nn - 1;
-            } else {
-                x = x * x;
-                nn = nn / 2;
-            }
-        }
-        if (n < 0)
-            ans = (double)(1.0) / (double)(ans);
-        return ans;
+        // if (nn < 0)
+        //     nn = -1 * nn;
+        // while (nn) {
+        //     if (nn % 2) {
+        //         ans = ans * x;
+        //         nn = nn - 1;
+        //     } else {
+        //         x = x * x;
+        //         nn = nn / 2;
+        //     }
+        // }
+        // if (n < 0)
+        //     ans = (double)(1.0) / (double)(ans);
+        // return ans;
     }
 };
