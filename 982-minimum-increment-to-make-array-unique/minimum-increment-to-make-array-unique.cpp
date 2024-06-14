59ca86afc88dc2f5284b02cc375ef37495c1f7fd
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-06-14 12:00:55 +0530

    Time: 122 ms (52.95%) | Memory: 69.1 MB (50.39%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-
+        int n = nums.size();
         int mov = 0;
 
-        for (int i = 1; i < nums.size(); i++) {
+        for (int i = 1; i < n; i++) {
 
-            //we will check if previous element is
-            //greater than or equal to current element then 
-            //no. of mov will be diff b/w them +1 and we 
-            // we will update current element with previous 
-            //element +1
+            // we will check if previous element is
+            // greater than or equal to current element then
+            // no. of mov will be diff b/w them +1 and we
+            //  we will update current element with previous
+            // element +1
             if (nums[i - 1] >= nums[i]) {
                 mov += nums[i - 1] - nums[i] + 1;
                 nums[i] = nums[i - 1] + 1;
