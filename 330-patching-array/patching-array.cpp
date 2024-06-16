c22de94a88f276a664ca7847e6c91e4bb4ea355e
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-06-16 15:31:56 +0530

    Time: 3 ms (83.44%) | Memory: 13.8 MB (45.41%) - LeetSync

--- 330-patching-array/patching-array.cpp
+++ 330-patching-array/patching-array.cpp
@@ -6,11 +6,15 @@ public:
         long long patch = 0;
 
         while (missing <= n) {
+
+            //check missing is less than nums[i] or not 
             if(i< nums.size() && nums[i]<=missing){
                 missing = missing + nums[i];
                 i++;
 
             }
+
+            // when missing is less than nums[i] that means missing is not covered and we patch it and check further
             else {
                 missing += missing;
                 patch++;
