13c1b2746acc34656e4d2213ef7f6733a9746a9d
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-06-16 23:41:26 +0530

    Time: 150 ms (50.00%) | Memory: 138.1 MB (50.00%) - LeetSync

--- 3418-count-pairs-that-form-a-complete-day-ii/count-pairs-that-form-a-complete-day-ii.cpp
+++ 3418-count-pairs-that-form-a-complete-day-ii/count-pairs-that-form-a-complete-day-ii.cpp
@@ -6,13 +6,21 @@ public:
         long long cnt = 0;
         int n = hours.size();
 
-        unordered_map<int, int> mpp;
+        map<int, int> mpp;
 
         for (int i = 0; i < n; i++) {
+
+            // finding the reminder of 24 for each element
             long long rem = hours[i] % 24;
+            // finding the comparative part to make it 0 when mod 24
             long long comp = (24 - rem) % 24;
+
+            // if comp is already in map then we find one pair of complete day
             if(mpp.find(comp)!=mpp.end())
+            // we update our no. of complete day count by frequency of comp in map
             cnt+=mpp[comp];
+
+            // else we will increase frquency of rem in map
             mpp[rem]++;
 
         }
