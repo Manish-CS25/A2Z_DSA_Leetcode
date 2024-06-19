9b754f0a186661b13e5ef2938affd29a9ad8cc58
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-06-19 15:57:19 +0530

    Time: 135 ms (5.53%) | Memory: 69 MB (28.78%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    public:
+public:
     bool possible(vector<int>& bloomDay, int days, int m, int k) {
         int count = 0;
         int no_b = 0;
@@ -21,15 +21,12 @@ public:
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n = bloomDay.size();
         long long val = m * 1ll * k * 1ll;
-        
+
         if (n < val) {
             return -1;
         }
         int min = *min_element(bloomDay.begin(), bloomDay.end());
         int max = *max_element(bloomDay.begin(), bloomDay.end());
-
-
-
 
         // for (int i = min; i <= max; i++) {
         //     if (possible(bloomDay, i, m, k)) {
@@ -37,25 +34,18 @@ public:
         //     }
         // }
 
-
         // ==========commented code is linear search and gives TLE========
-
 
         // here is the binary approach
 
         int low = min, high = max;
         while (low <= high) {
-        int mid = (low + high) / 2;
-        if (possible(bloomDay, mid, m, k)) {
-            high = mid - 1;
-        }
-        else low = mid + 1;
+            int mid = (low + high) / 2;
+            if (possible(bloomDay, mid, m, k)) {
+                high = mid - 1;
+            } else
+                low = mid + 1;
         }
         return low;
-
-
-        
     }
-
-
 };
