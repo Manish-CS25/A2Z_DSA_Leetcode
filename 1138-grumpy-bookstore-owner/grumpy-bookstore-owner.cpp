388ae5703e4fd2391c2111e9454014a390848d91
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-06-21 16:24:15 +0530

    Time: 404 ms (5.01%) | Memory: 34.2 MB (97.12%) - LeetSync

---
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+
+        int default_satisfied = 0;
+
+        int max_window_sum = INT_MIN;
+        int n = customers.size();
+        //// count satisfied customers initially when store keeper is not grumpy
+        ///mean grumpy[i]==0
+        for (int i = 0; i < n; i++) {
+            if (grumpy[i] == 0)
+                default_satisfied += customers[i];
+        }
+
+        // now find a sub array of minutes(k) in customers for which customer
+        // satisfication is greater even if store keeper is grumpy
+
+        for (int i = 0; i < (n - minutes + 1); i++) {
+            int window_sum = 0;
+            for (int j = 0; j < minutes; j++) {
+                if (grumpy[i + j] == 1) {
+                    window_sum += customers[i + j];
+                }
+                max_window_sum = max(max_window_sum, window_sum);
+            }
+        }
+
+        return default_satisfied + max_window_sum;
+    }
+};
