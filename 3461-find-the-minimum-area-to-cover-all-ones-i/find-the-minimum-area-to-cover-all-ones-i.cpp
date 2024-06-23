08d15b780db8cb5d3402489129655ae821f1f534
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-06-23 18:03:18 +0530

    Time: 283 ms (14.29%) | Memory: 132.2 MB (28.57%) - LeetSync

--- 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -12,9 +12,17 @@ public:
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
+
+                    // min index of left --column wise
                     L = min(L, j);
+
+                    // max index of right --column wise
                     R = max(R, j);
+
+                    // min index of upper--Row wise
                     U = min(U, i);
+
+                    // max index of down --Row wise
                     D = max(D, i);
                 }
             }
