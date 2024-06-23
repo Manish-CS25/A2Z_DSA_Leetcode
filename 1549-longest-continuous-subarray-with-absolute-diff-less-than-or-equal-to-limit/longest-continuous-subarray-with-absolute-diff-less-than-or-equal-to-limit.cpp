d04c22bb60372936a80f78216a23bf0cb65d642a
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-06-23 17:41:11 +0530

    Time: 57 ms (91.65%) | Memory: 54.5 MB (69.11%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,5 +1,44 @@
 class Solution {
 public:
+    // Approach
+
+    // Initialization:
+    //     Create two pointers, l and r, both starting at the beginning of the
+    //     array. Initialize two deques, mindq and maxdq, to keep track of the
+    //     minimum and maximum values in the current window. Initialize a
+    //     variable maxsize to store the maximum length of the valid subarray
+    //     found.
+
+    // Sliding Window:
+    //     Use a while loop to iterate with the right pointer r until it reaches
+    //     the end of the array.
+
+    // Update Deques:
+    //     Max Deque: Remove elements from the back of maxdq that are smaller
+    //     than the current element nums[r] (to maintain decreasing order). Min
+    //     Deque: Remove elements from the back of mindq that are greater than
+    //     the current element nums[r] (to maintain increasing order). Add the
+    //     current element nums[r] to both deques.
+
+    // Check Validity:
+    //     If the difference between the front elements of maxdq and mindq
+    //     exceeds the limit, it means the current window is invalid:
+    //         Increment the left pointer l.
+    //         Remove the front element from mindq if it is equal to nums[l]
+    //         (since we are shrinking the window from the left). Remove the
+    //         front element from maxdq if it is equal to nums[l].
+
+    // Update Maximum Size:
+    //     Calculate the size of the current valid window (r - l + 1) and update
+    //     maxsize if it is larger than the previously recorded maximum size.
+
+    // Move Right Pointer:
+    //     Increment the right pointer r to expand the window to the right.
+
+    // Return Result:
+    //     After exiting the loop, maxsize will contain the length of the
+    //     longest subarray that satisfies the condition.
+
     int longestSubarray(vector<int>& nums, int limit) {
         int n = nums.size();
 
@@ -9,7 +48,7 @@ public:
 
         int maxsize = 1;
 
-        while (r<n) {
+        while (r < n) {
             while (!maxdq.empty() && nums[r] > maxdq.back()) {
                 maxdq.pop_back();
             }
