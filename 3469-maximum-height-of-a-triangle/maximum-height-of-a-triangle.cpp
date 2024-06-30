db4892bc430bfab009985327761c068578b8acc1
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-06-30 23:11:05 +0530

    Time: 4 ms (37.50%) | Memory: 7.4 MB (62.50%) - LeetSync

--- 3469-maximum-height-of-a-triangle/maximum-height-of-a-triangle.cpp
+++ 3469-maximum-height-of-a-triangle/maximum-height-of-a-triangle.cpp
@@ -2,20 +2,28 @@ class Solution {
 public:
     int maximizer(int ball1, int ball2) {
         int h = 0;
-        for (int i = 1; ; i++) {
+        for (int i = 1;; i++) {
+
+            // subtracting odd no. of ball2 , because balls are either odd or
+            // even order
             ball1 -= 2 * i - 1;
             if (ball1 >= 0)
                 h++;
             else
                 break;
-
-            ball2 -= 2 * i; if (ball2 >= 0) h++;
-            else break;
+            // subtracting even no.of ball2, because balls are either odd or
+            // even order
+            ball2 -= 2 * i;
+            if (ball2 >= 0)
+                h++;
+            else
+                break;
         }
         return h;
     }
     int maxHeightOfTriangle(int red, int blue) {
-
+        // just taking red and blue ball as first one by one check for maximum
+        // height
         return max(maximizer(red, blue), maximizer(blue, red));
     }
 };
