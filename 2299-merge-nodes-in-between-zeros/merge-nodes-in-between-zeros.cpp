dc703a5cfaaa0e78ee59f035950d116ab3fbd5ba
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-07-04 17:28:37 +0530

    Time: 435 ms (64.46%) | Memory: 256.5 MB (96.69%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
@@ -42,26 +42,49 @@
 // };
 
 // =============Recursive Solution ===============
-class Solution {
+
+
+// class Solution {
+// public:
+//     ListNode* mergeNodes(ListNode* head) {
+//         // BASE CASE -> if we have a single zero, simply return null
+//         if (!head->next)
+//             return nullptr;
+
+//         // fetch sum from current 0 to next 0
+//         ListNode* ptr = head->next;
+//         int sum = 0;
+//         while (ptr->val != 0) {
+//             sum += ptr->val;
+//             ptr = ptr->next;
+//         }
+//         // assign sum on the first node between nodes having value 0.
+//         head->next->val = sum;
+//         // call and get the answer and connect the answer to next of head->next
+
+//         head->next->next = mergeNodes(ptr);
+//         // return head->next..=> new head
+//         return head->next;
+//     }
+// };
+
+
+
+// ==============Iterative solution ==========
+class Solution{
 public:
-    ListNode* mergeNodes(ListNode* head) {
-        // BASE CASE -> if we have a single zero, simply return null
-        if (!head->next)
-            return nullptr;
-
-        // fetch sum from current 0 to next 0
-        ListNode* ptr = head->next;
-        int sum = 0;
-        while (ptr->val != 0) {
-            sum += ptr->val;
-            ptr = ptr->next;
-        }
-        // assign sum on the first node between nodes having value 0.
-        head->next->val = sum;
-        // call and get the answer and connect the answer to next of head->next
-
-        head->next->next = mergeNodes(ptr);
-        // return head->next..=> new head
-        return head->next;
-    }
+ ListNode* mergeNodes(ListNode* head) {
+     head=head->next;
+     ListNode* start=head;
+     while(start){
+	    ListNode* end= start;   /* Point to first node of current part for getting sum */
+        int sum=0;
+        while(end->val!=0) sum+= end->val , end=end->next;
+        start->val=sum;   /*assign sum to first node between two 0*/
+        start->next=end->next;   /*make this connect to first node of next part*/
+        start=start->next;    /*go..to..next..part*/
+	 }
+    
+     return head;
+ }
 };
