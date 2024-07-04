768a95e6a9ef6d5d1b3b3727f18ee0b13a8377c5
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-07-04 17:24:49 +0530

    Time: 423 ms (73.97%) | Memory: 259.7 MB (67.92%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
@@ -8,36 +8,62 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-    ListNode* mergeNodes(ListNode* head) {
-        ListNode* new_Head;
-        int sum = 0;
-        ListNode* temp = head->next;
-        int i=0;
-        ListNode* temp3; 
-        while (temp != NULL) {
-            if (temp->val != 0)
-                sum += temp->val;
-            else {
-                
-                ListNode* tmp2 = new ListNode(sum);
-                if(i==0){
-                    new_Head = tmp2;
-                    temp3 = new_Head;
-                    i++;
-                }
-                
-                else{
-                    temp3->next= tmp2;
-                    temp3 = temp3->next;
-                }
-                
-                sum = 0;
-  
+// class Solution {
+// public:
+//     ListNode* mergeNodes(ListNode* head) {
+//         ListNode* new_Head;
+//         int sum = 0;
+//         ListNode* temp = head->next;
+//         int i = 0;
+//         ListNode* temp3;
+//         while (temp != NULL) {
+//             if (temp->val != 0)
+//                 sum += temp->val;
+//             else {
+
+//                 ListNode* tmp2 = new ListNode(sum);
+//                 if (i == 0) {
+//                     new_Head = tmp2;
+//                     temp3 = new_Head;
+//                     i++;
+//                 }
+
+//                 else {
+//                     temp3->next = tmp2;
+//                     temp3 = temp3->next;
+//                 }
+
+//                 sum = 0;
+//             }
+//             temp = temp->next;
+//         }
+//         return new_Head;
+//     }
+// };
+
+
+
+
+// =============Recursive Solution ===============
+class Solution{
+    public:
+         ListNode* mergeNodes(ListNode* head){
+            if(!head->next) return nullptr;
+
+            ListNode* ptr = head->next;
+            int sum=0;
+            while(ptr->val!=0)
+            {
+                sum+=ptr->val;
+                ptr= ptr->next;
             }
-            temp = temp->next;
-        }
-        return new_Head;
-    }
+
+            head->next->val= sum;
+
+            head->next->next = mergeNodes(ptr);
+
+            return head->next;
+         }
+
+
 };
