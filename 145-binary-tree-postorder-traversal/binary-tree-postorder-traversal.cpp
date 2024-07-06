3ed0a7307dc4596103a1ef4c19d2b542b8be7022
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-07-06 14:55:49 +0530

    Time: 4 ms (31.10%) | Memory: 9.7 MB (88.45%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -28,29 +28,61 @@
 
 // Iterative solution using 2 Stacks
 
+// class Solution {
+// public:
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> p;
+//         if (root == NULL)
+//             return p;
+//         stack<TreeNode*> st1, st2;
+//         st1.push(root);
+//         while (!st1.empty()) {
+//  root = st1.top();
+//             st1.pop();
+//             st2.push(root);
+//             if (root->left != NULL)
+//                 st1.push(root->left);
+//             if (root->right != NULL)
+//                 st1.push(root->right);
+//         }
+
+//         while (!st2.empty()) {
+//             p.push_back(st2.top()->val);
+//             st2.pop();
+//         }
+//         return p;
+//     }
+// };
+
+// solution using 1 stack
+
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> p;
-        if (root == NULL)
-            return p;
-        stack<TreeNode*> st1, st2;
-        st1.push(root);
-        while (!st1.empty()) {
 
-            root = st1.top();
-            st1.pop();
-            st2.push(root);
-            if (root->left != NULL)
-                st1.push(root->left);
-            if (root->right != NULL)
-                st1.push(root->right);
-        }
+        vector<int> post_order;
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        while (!st.empty() || curr != NULL) {
 
-        while (!st2.empty()) {
-            p.push_back(st2.top()->val);
-            st2.pop();
+            if (curr != NULL) {
+                st.push(curr);
+                curr = curr->left;
+            } else {
+                TreeNode* temp = st.top()->right;
+                if (temp == NULL) {
+                    temp = st.top();
+                    st.pop();
+                    post_order.push_back(temp->val);
+                    while (!st.empty() && temp == st.top()->right) {
+                        temp = st.top();
+                        st.pop();
+                        post_order.push_back(temp->val);
+                    }
+                } else
+                    curr = temp;
+            }
         }
-        return p;
+        return post_order;
     }
 };
