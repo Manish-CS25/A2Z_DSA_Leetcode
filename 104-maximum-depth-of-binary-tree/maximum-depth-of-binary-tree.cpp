13fcc920cfcdac727df8948ba8efb769e0cfa86e
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-07-08 13:39:20 +0530

    Time: 4 ms (77.14%) | Memory: 17.6 MB (11.44%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -10,11 +10,38 @@
  * right(right) {}
  * };
  */
+
+// ==================DFS recursive Solution ========================
+// class Solution {
+// public:
+//     int maxDepth(TreeNode* root) {
+//         return root == NULL
+//                    ? 0
+//                    : max(maxDepth(root->left), maxDepth(root->right)) + 1;
+//     }
+// };
+
+// ================BFS iterative Solution ==========================
+
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        return root == NULL
-                   ? 0
-                   : max(maxDepth(root->left), maxDepth(root->right)) + 1;
+        if (root==NULL) return 0; 
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth = 0;
+        while (!q.empty()) {
+            depth++;
+            int s = q.size();
+            for (int i = 0; i < s; i++) {
+                TreeNode* front = q.front();
+                q.pop();
+                if (front->left)
+                    q.push(front->left);
+                if (front->right)
+                    q.push(front->right);
+            }
+        }
+        return depth;
     }
 };
