73b92d78c7bc34a77681ad9a264a01d2ce02ad88
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-07-08 13:41:50 +0530

    Time: 4 ms (77.14%) | Memory: 17.7 MB (11.44%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -23,25 +23,44 @@
 
 // ================BFS iterative Solution ==========================
 
-class Solution {
-public:
+// class Solution {
+// public:
+//     int maxDepth(TreeNode* root) {
+//         if (root==NULL) return 0; 
+//         queue<TreeNode*> q;
+//         q.push(root);
+//         int depth = 0;
+//         while (!q.empty()) {
+//             depth++;
+//             int s = q.size();
+//             for (int i = 0; i < s; i++) {
+//                 TreeNode* front = q.front();
+//                 q.pop();
+//                 if (front->left)
+//                     q.push(front->left);
+//                 if (front->right)
+//                     q.push(front->right);
+//             }
+//         }
+//         return depth;
+//     }
+// };
+
+// ================DFS iterative Solution ==========================
+class Solution{
+    public:
     int maxDepth(TreeNode* root) {
-        if (root==NULL) return 0; 
-        queue<TreeNode*> q;
-        q.push(root);
-        int depth = 0;
-        while (!q.empty()) {
-            depth++;
-            int s = q.size();
-            for (int i = 0; i < s; i++) {
-                TreeNode* front = q.front();
-                q.pop();
-                if (front->left)
-                    q.push(front->left);
-                if (front->right)
-                    q.push(front->right);
-            }
+        if (root == NULL) return 0;
+        stack<pair<TreeNode*, int>> s;
+        s.push({root, 1});
+        int len = 1;
+        while (!s.empty()) {
+            pair<TreeNode*, int> front = s.top(); s.pop();
+            len = max(len, front.second);
+            if (front.first->left) s.push({front.first->left, front.second+1});
+            if (front.first->right) s.push({front.first->right, front.second+1});
         }
-        return depth;
+        return len;
     }
 };
+
