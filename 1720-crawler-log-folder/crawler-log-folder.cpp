7d26c1aff7fa64fb9e2b9adf7ee9bdcc494f28ca
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-07-10 13:55:24 +0530

    Time: 5 ms (46.30%) | Memory: 13.9 MB (84.88%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -3,12 +3,11 @@ public:
     int minOperations(vector<string>& logs) {
         int minOp = 0;
         int n = logs.size();
-        for(int i=0;i<n;i++){
-            if(logs[i]!="../" && logs[i]!="./")
-            minOp++;
-            if(logs[i]=="../" && minOp !=0)
-            minOp--;
-        
+        for (int i = 0; i < n; i++) {
+            if (logs[i] != "../" && logs[i] != "./")
+                minOp++;
+            if (logs[i] == "../" && minOp != 0)
+                minOp--;
         }
         return minOp;
     }
