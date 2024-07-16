69fa154d7fa7b4756a8174f738d9c7d7a4e8688a
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-07-16 18:08:09 +0530

    Time: 15 ms (40.29%) | Memory: 22.4 MB (12.07%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -10,29 +10,50 @@
  * right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int height(TreeNode* root) {
+//         if (root == NULL) {
+//             return 0;
+//         }
+
+//         int left = height(root->left);
+//         int right = height(root->right);
+
+//         int ans = max(left, right) + 1;
+//         return ans;
+//     }
+//     int diameterOfBinaryTree(TreeNode* root) {
+
+//         if (root == NULL) {
+//             return 0;
+//         }
+//         int left_d = diameterOfBinaryTree(root->left);
+//         int right_d = diameterOfBinaryTree(root->right);
+//         int ans = height(root->left) + height(root->right);
+//         int d = max(left_d, max(right_d, ans));
+
+//         return d;
+//     }
+// };
+
+// ===============Optimal Solution ===========================
 class Solution {
 public:
-    int height(TreeNode* root) {
-        if (root == NULL) {
+    int height(TreeNode* Node, int& d) {
+        if (Node == NULL) {
             return 0;
         }
 
-        int left = height(root->left);
-        int right = height(root->right);
-
-        int ans = max(left, right) + 1;
-        return ans;
+        int LH = height(Node->left, d);
+        int RH = height(Node->right, d);
+        d = max(d, LH + RH);
+        return 1 + max(LH, RH);
     }
+
     int diameterOfBinaryTree(TreeNode* root) {
-
-        if (root == NULL) {
-            return 0;
-        }
-        int left_d = diameterOfBinaryTree(root->left);
-        int right_d = diameterOfBinaryTree(root->right);
-        int ans = height(root->left) + height(root->right);
-        int d = max(left_d, max(right_d, ans));
-
+        int d = 0;
+        height(root, d);
         return d;
     }
 };
