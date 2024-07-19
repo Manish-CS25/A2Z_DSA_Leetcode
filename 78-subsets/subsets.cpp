802f1203f64852aa33aea91a37a8e9be4c7f5154
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-07-19 23:19:55 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (13.76%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,18 +1,47 @@
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>> powerset;
+//         int n = nums.size();
+
+//         int size = (1 << n) - 1;
+//         for (int i = 0; i <= size; i++) {
+//             vector<int> sub = {};
+//             for (int j = 0; j < n; j++) {
+//                 if (i & (1 << j))
+//                     sub.push_back(nums[j]);
+//             }
+//             powerset.push_back(sub);
+//         }
+//         return powerset;
+//     }
+// };
+
+// ====================Optimal Solution using backtracking=======
+
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> powerset;
-        int n = nums.size();
-      
-        int size = (1 << n) - 1;
-        for (int i = 0; i <= size; i++) {
-            vector<int> sub = {};
-            for (int j = 0; j < n; j++) {
-                if (i & (1 << j))
-                    sub.push_back(nums[j]);
-            }
-            powerset.push_back(sub);
+    void setMaker(int index, vector<int>& nums, vector<vector<int>>& powerset,
+                  vector<int> subset, int n) {
+        if (index == n) {
+            powerset.push_back(subset);
+            return;
         }
+
+        // take element
+        subset.push_back(nums[index]);
+        setMaker(index + 1, nums, powerset, subset, n);
+
+        // not take element
+        subset.pop_back();
+        setMaker(index + 1, nums, powerset, subset, n);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> powerset;
+
+        setMaker(0, nums, powerset, {}, n);
+
         return powerset;
     }
 };
