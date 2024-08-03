d821b213c32a961ddc0af25b109756bc225891ad
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-08-03 15:11:10 +0530

    Time: 24 ms (5.23%) | Memory: 17.1 MB (96.22%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,27 +1,37 @@
+// class Solution {
+// public:
+//     bool canBeEqual(vector<int>& target, vector<int>& arr) {
+//         int n = arr.size();
+
+//         unordered_map<int, int> mp;
+
+//         for (int i = 0; i < n; i++) {
+
+//             mp[arr[i]]++;
+//         }
+
+//         for (int j = 0; j < n; j++) {
+//             if (mp.find(target[j]) == mp.end())
+//                 return false;
+//             mp[target[j]]--;
+//         }
+
+//         for (int i = 0; i < n; i++) {
+
+//             if (mp[arr[i]] != 0)
+//                 return false;
+//         }
+
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        int n = arr.size();
-
-        unordered_map<int, int> mp;
-
-        for (int i = 0; i < n; i++) {
-
-            mp[arr[i]]++;
-        }
-
-        for (int j = 0; j < n; j++) {
-            if (mp.find(target[j]) == mp.end())
-                return false;
-            mp[target[j]]--;
-        }
-
-        for (int i = 0; i < n; i++) {
-
-            if (mp[arr[i]] != 0)
-                return false;
-        }
-
-        return true;
+        return is_permutation(target.begin(), target.end(),arr.begin());
     }
 };
+
+
+
