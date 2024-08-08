360f55800854632c692144cebb6cbebd6a4e692c
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-08-08 23:13:51 +0530

    Time: 29 ms (45.59%) | Memory: 38.4 MB (58.54%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -5,11 +5,16 @@ public:
         int n = nums.size();
 
         vector<int> ans(n, 1);
+
+        // In  first pass just store prefix product in ans[i] (left->right)
         int curr = 1;
         for (int i = 0; i < n; i++) {
             ans[i] *= curr;
             curr *= nums[i];
         }
+
+        // In  second pass just store  suffix product that would multilpy with
+        // already existed prefix product (right->left)
         curr = 1;
 
         for (int i = n - 1; i >= 0; i--) {
