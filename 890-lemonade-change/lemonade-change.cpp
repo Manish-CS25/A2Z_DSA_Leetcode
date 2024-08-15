a0946aaa9d13156c7aa499221a0b4fa9c919e384
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-08-15 17:59:43 +0530

    Time: 60 ms (92.98%) | Memory: 86.1 MB (32.71%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -2,6 +2,39 @@ class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
 
+        // int fives = 0;
+        // int tens = 0;
+
+        // int n = bills.size();
+        // for (int i = 0; i < n; i++) {
+        //     if (bills[i] == 5)
+        //         fives++;
+        //     if (bills[i] == 10) {
+        //         tens++;
+        //         if (fives)
+        //             fives--;
+        //         else
+        //             return false;
+        //     }
+        //     if (bills[i] == 20) {
+        //         if (!fives)
+        //             return false;
+
+        //         else if (tens >= 1 && fives >= 1) {
+        //             tens -= 1;
+        //             fives -= 1;
+
+        //         } else if (fives >= 3) {
+        //             fives -= 3;
+
+        //         }
+
+        //         else
+        //             return false;
+        //     }
+        // }
+        // return true;
+
         int fives = 0;
         int tens = 0;
 
@@ -10,25 +43,22 @@ public:
             if (bills[i] == 5)
                 fives++;
             if (bills[i] == 10) {
-                tens++;
-                if (fives)
-                    fives--;
+                if (fives>=1){
+                   fives--;
+                   tens++; 
+                }
+                    
                 else
                     return false;
             }
+
             if (bills[i] == 20) {
-                if (!fives)
-                    return false;
+                if (tens && fives) {
+                    tens--;
+                    fives--;
 
-                else if (tens >= 1 && fives >= 1) {
-                    tens -= 1;
-                    fives -= 1;
-
-                } else if (fives >= 3) {
+                } else if (fives >= 3)
                     fives -= 3;
-
-                }
-
                 else
                     return false;
             }
