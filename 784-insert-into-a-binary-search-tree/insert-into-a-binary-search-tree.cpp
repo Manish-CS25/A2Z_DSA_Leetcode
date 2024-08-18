9b6d84dd74a63af8a09ca03955d179cbad81ea3b
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-08-18 23:43:02 +0530

    Time: 63 ms (62.19%) | Memory: 57.5 MB (36.19%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
@@ -10,32 +10,53 @@
  * right(right) {}
  * };
  */
+
+
+ //==============My Approach ===============
+// class Solution {
+// public:
+//     TreeNode* insertIntoBST(TreeNode* root, int val) {
+//         TreeNode* newnode = new TreeNode(val);
+//         if (root == NULL) {
+//             return newnode;
+//         }
+//         TreeNode* temp = root;
+
+//         while (temp) {
+//             if (temp->left == NULL && val < temp->val) {
+//                 temp->left = newnode;
+//                 break;
+//             }
+
+//             if (temp->right == NULL && val > temp->val) {
+//                 temp->right = newnode;
+//                 break;
+//             }
+
+//             if (val > temp->val) {
+//                 temp = temp->right;
+
+//             } else
+//                 temp = temp->left;
+//         }
+//         return root;
+//     }
+// };
+
+
+// ==============Recursive Approach ==============
 class Solution {
 public:
-    TreeNode* insertIntoBST(TreeNode* root, int val) {
-        TreeNode* newnode = new TreeNode(val);
-        if (root == NULL) {
-            return newnode;
+    TreeNode* insertIntoBST(TreeNode* node, int val) {
+        if (!node) {
+            TreeNode* newNode = new TreeNode(val);
+            return newNode;
         }
-        TreeNode* temp = root;
-
-        while (temp) {
-            if (temp->left == NULL && val < temp->val) {
-                temp->left = newnode;
-                break;
-            }
-
-            if (temp->right == NULL && val > temp->val) {
-                temp->right = newnode;
-                break;
-            }
-
-            if (val > temp->val) {
-                temp = temp->right;
-
-            } else
-                temp = temp->left;
+        if (val < node->val) {
+            node->left = insertIntoBST(node->left, val);
+        } else {
+            node->right = insertIntoBST(node->right, val);
         }
-        return root;
+        return node;
     }
 };
