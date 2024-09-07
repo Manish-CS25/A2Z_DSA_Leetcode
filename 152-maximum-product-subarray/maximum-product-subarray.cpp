66c104b1bce2fb2c75c316138de35c45e1a27448
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-09-08 03:32:33 +0530

    Time: 10 ms (19.36%) | Memory: 16.7 MB (46.48%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        long long maxi = INT_MIN;
+        // long long maxi = INT_MIN;
+        // int n = nums.size();
+        // long long pro = 1;
+
+        // for (int i = 0; i < n; i++) {
+        //     pro = pro * nums[i];
+        //     maxi = max(maxi, pro);
+        //     if (pro == 0)
+        //         pro = 1;
+        // }
+
+        // //// Second pass to check backward
+        //  pro = 1;
+        // for (int i = n - 1; i >= 0; i--) {
+        //     pro = pro * nums[i];
+        //     maxi = max(maxi, pro);
+        //     if (pro == 0)
+        //         pro = 1;
+        // }
+
+        // return maxi;
+
+        // in one pass
+
+        int prefix = 1, suffix = 1;
         int n = nums.size();
-        long long pro = 1;
+        int maxi = INT_MIN;
 
         for (int i = 0; i < n; i++) {
-            pro = pro * nums[i];
-            maxi = max(maxi, pro);
-            if (pro == 0)
-                pro = 1;
-        }
+            if (prefix == 0)
+                prefix = 1;
+            if (suffix == 0)
+                suffix = 1;
 
-        //// Second pass to check backward
-         pro = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            pro = pro * nums[i];
-            maxi = max(maxi, pro);
-            if (pro == 0)
-                pro = 1;
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[n - i - 1];
+            maxi = max(maxi, max(prefix, suffix));
         }
 
         return maxi;
