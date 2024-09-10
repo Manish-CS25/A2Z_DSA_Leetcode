ccbcfdaa87eb092a9f089ee597c551d7bb49f476
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-09-10 12:35:35 +0530

    Time: 12 ms (16.56%) | Memory: 15.6 MB (69.91%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
+    // Custom comparator to decide the order of concatenation
     static bool comp(int a, int b) {
         string str_a = to_string(a);
         string str_b = to_string(b);
         return str_a + str_b > str_b + str_a;
     }
-    string largestNumber(vector<int>& nums) {
 
+    string largestNumber(vector<int>& nums) {
+        // Sort numbers using the custom comparator
         sort(nums.begin(), nums.end(), comp);
+
+        // Create the largest number by concatenating sorted numbers
         string ans = "";
         for (auto it : nums) {
             ans += to_string(it);
         }
+
+        // Handle edge case where all numbers are zero
         if (ans[0] == '0')
             return "0";
 
