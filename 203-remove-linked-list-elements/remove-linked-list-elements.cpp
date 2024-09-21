0f8e1ea8b87e49327abad082d75f0b4d2c8956be
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-09-22 02:48:55 +0530

    Time: 17 ms (49.73%) | Memory: 20 MB (74.05%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/remove-linked-list-elements.cpp
@@ -13,28 +13,18 @@ public:
     ListNode* removeElements(ListNode* head, int val) {
         if(head==NULL)
         return head;
-        // ListNode* dummy = new ListNode(-1);
-        // dummy->next = head;
-        // ListNode* temp = dummy;
-        // while (temp->next) {
-        //     if (temp->next->val == val) {
-        //         temp->next = temp->next->next;
-
-        //     } else
-        //         temp = temp->next;
-        // }
-        // return dummy->next;
-
-        while(head!=NULL && head->val ==val) head = head->next;
-                ListNode* temp = head;
-        while (temp) {
-            if (temp->next!=NULL && temp->next->val == val) {
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* temp = dummy;
+        while (temp->next) {
+            if (temp->next->val == val) {
                 temp->next = temp->next->next;
 
             } else
                 temp = temp->next;
         }
-        return head;
+        return dummy->next;
+
 
 
 
