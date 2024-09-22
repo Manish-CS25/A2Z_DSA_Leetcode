7bbe863a01b05d056c706126268bd9aadab98aec
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-09-22 16:11:21 +0530

    Time: 4 ms (84.62%) | Memory: 25.1 MB (82.72%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,15 +1,33 @@
+// class Solution {
+// public:
+//     vector<int> getSneakyNumbers(vector<int>& nums) {
+//         unordered_map<int, int> mp;
+//         for (auto it : nums) {
+//             mp[it]++;
+//         }
+
+//         vector<int> ans;
+//         for (auto it : mp) {
+//             if (it.second > 1) {
+//                 ans.push_back(it.first);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for (auto it : nums) {
-            mp[it]++;
-        }
-
         vector<int> ans;
-        for (auto it : mp) {
-            if (it.second > 1) {
-                ans.push_back(it.first);
+        int i = 0, d = 0;
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                ans.push_back(nums[i]);
             }
         }
         return ans;
