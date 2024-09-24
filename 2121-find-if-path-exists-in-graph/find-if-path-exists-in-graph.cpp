2fb167307218aeeb36a2d111e9d5cf99fdb7e9c1
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-09-24 12:59:59 +0530

    Time: 695 ms (77.43%) | Memory: 284.8 MB (69.72%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
-    bool bfs(int s, int d, vector<int>& vis, vector<vector<int>>& adj,
-             queue<int>& q) {
+    bool validPath(int n, vector<vector<int>>& edges, int s, int d) {
+
+        vector<vector<int>> adj(n);
+        int m = edges.size();
+        for (int i = 0; i < m; i++) {
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+        }
+
+        vector<int> vis(n, 0);
+        queue<int> q;
+        q.push(s);
         vis[s] = 1;
-        //    q.push(s);
+
         while (!q.empty()) {
             int node = q.front();
             q.pop();
@@ -12,31 +22,10 @@ public:
             for (auto it : adj[node]) {
                 if (vis[it] != 1) {
                     q.push(it);
-                    vis[it]=1;
+                    vis[it] = 1;
                 }
             }
         }
         return false;
     }
-
-
-
-
-    bool validPath(int n, vector<vector<int>>& edges, int s,
-                   int d) {
-
-    vector<vector<int>> adj(n);
-   int m = edges.size();
-    for (int i = 0; i < m; i++) {
-        adj[edges[i][0]].push_back(edges[i][1]);
-        adj[edges[i][1]].push_back(edges[i][0]);
-    }
-
-    vector<int> vis(n, 0);
-    queue<int> q;
-    q.push(s);
-
-    return bfs(s, d, vis, adj, q);
-}
-}
-;
+};
