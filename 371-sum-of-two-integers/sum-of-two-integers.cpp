091adbd24c6dcfdfab0725b9ade24b27a9a7a9a6
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-09-29 01:55:25 +0530

    Time: 2 ms (56.27%) | Memory: 7.4 MB (23.66%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -2,7 +2,15 @@ class Solution {
 public:
     int getSum(int a, int b) {
 
-        return a + b;
-        
+        unsigned int carry = a & b;
+        int sum_without_carry = a ^ b;
+        int actual_carry = carry << 1;
+        while (carry != 0) {
+            carry = sum_without_carry & actual_carry;
+            sum_without_carry = sum_without_carry ^ actual_carry;
+            actual_carry = carry << 1;
+        }
+
+        return sum_without_carry;
     }
 };
