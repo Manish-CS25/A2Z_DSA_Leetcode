bbcaaa4c3de3a5e002014f02c13d77a87b3cf445
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-09-30 02:03:34 +0530

    Time: 105 ms (5.75%) | Memory: 96.5 MB (5.74%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -2,19 +2,25 @@ class Solution {
 public:
     int maxProfit(vector<int>& p) {
         int n = p.size();
-        if (p.size() == 1)
+        if (n == 1)
             return 0;
         int maxi = INT_MIN;
 
-        priority_queue<int> pq;
-        pq.push(p[n - 1]);
+        stack<int> st;
+        st.push(p[n - 1]);
 
         for (int i = n - 2; i >= 0; i--) {
-            maxi = max(maxi, pq.top() - p[i]);
+            while (!st.empty() && p[i] >= st.top()) {
+                st.pop();
+            }
+            if (st.empty())
+                st.push(p[i]);
 
-            pq.push(p[i]);
+            else
+
+                maxi = max(maxi, st.top() - p[i]);
         }
 
-        return maxi > 0 ? maxi : 0;
+        return maxi>0?maxi:0;
     }
 };
