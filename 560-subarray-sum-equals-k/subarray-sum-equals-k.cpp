cb41e61f910866409b38fe8424897c2acbe9190e
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-10-04 00:44:37 +0530

    Time: 91 ms (46.67%) | Memory: 47.7 MB (40.19%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,16 +1,44 @@
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int n=nums.size(),res=0;
+//         for(int i=0;i<n;i++){
+//             int sum=0;
+//             for(int j=i;j<n;j++){
+//                 sum+=nums[j];
+//                 if(sum==k){
+//                     res++;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+/// =========Optimal Way ===========
+
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n=nums.size(),res=0;
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                if(sum==k){
-                    res++;
-                }
+        int cnt = 0;
+        long long sum = 0;
+        map<long long, int> prefix; // To store the frequency of prefix sums
+        
+        prefix[0] = 1; // Initialize with prefix sum 0 occurring once (important for subarrays starting from index 0)
+
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+
+            // Check if there's a prefix sum that satisfies sum - k
+            long long rem = sum - k;
+            if (prefix.find(rem) != prefix.end()) {
+                cnt += prefix[rem];
             }
+
+            // Update the prefix sum frequency
+            prefix[sum]++;
         }
-        return res;
+
+        return cnt;
     }
 };
