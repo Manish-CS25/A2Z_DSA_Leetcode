df8b21a4795c26b4c8d4da9867b74506882ff107
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-10-04 20:33:21 +0530

    Time: 52 ms (95.00%) | Memory: 56.4 MB (84.20%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -1,3 +1,48 @@
+// class Solution {
+// public:
+//     long long dividePlayers(vector<int>& skill) {
+
+//         long long sum = 0;
+//         int n = skill.size();
+
+//         unordered_map<int, int> mp;
+
+//         for (int i = 0; i < n; i++) {
+//             mp[skill[i]]++;
+//             sum += skill[i];
+//         }
+
+//         int teams = n / 2;
+//         if (sum % teams != 0)
+//             return -1;
+//         int skill_count = sum / teams;
+
+//         sum = 0;
+//         for (int i = 0; i < n; i++) {
+//             int rem = skill_count - skill[i];
+//             if (mp.find(rem) != mp.end()) {
+//                 if (mp[skill[i]] > 0 && mp[rem] == 0)
+//                     return -1;
+
+//                 if (mp[skill[i]]) {
+
+//                     sum += skill[i] * rem;
+//                     mp[rem]--;
+//                     mp[skill[i]]--;
+//                 }
+
+//             } else
+//                 return -1;
+//         }
+
+//         return sum ;
+//     }
+// };
+
+
+
+// ==================Another Optimal Approach ============
+
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
@@ -5,10 +50,9 @@ public:
         long long sum = 0;
         int n = skill.size();
 
-        unordered_map<int, int> mp;
 
         for (int i = 0; i < n; i++) {
-            mp[skill[i]]++;
+
             sum += skill[i];
         }
 
@@ -16,24 +60,21 @@ public:
         if (sum % teams != 0)
             return -1;
         int skill_count = sum / teams;
+        sort(skill.begin(), skill.end());
+        int left = 0, right = n-1;
 
         sum = 0;
-        for (int i = 0; i < n; i++) {
-            int rem = skill_count - skill[i];
-            if (mp.find(rem) != mp.end()) {
-                if (mp[skill[i]] > 0 && mp[rem] == 0)
-                    return -1;
 
-                if (mp[skill[i]]) {
+        while(left<right){
+            if(skill[left]+skill[right]!=skill_count)
+            return -1;
 
-                    sum += skill[i] * rem;
-                    mp[rem]--;
-                    mp[skill[i]]--;
-                }
+            else sum+= skill[left]*skill[right];
 
-            } else
-                return -1;
+            left++;
+            right--;
         }
+
 
         return sum ;
     }
