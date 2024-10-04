6df390b1976892c557541c354070af1b91956a7b
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-10-04 20:19:14 +0530

    Time: 65 ms (53.00%) | Memory: 57.3 MB (11.00%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -24,7 +24,7 @@ public:
                 if (mp[skill[i]] > 0 && mp[rem] == 0)
                     return -1;
 
-                if (mp[rem] > 0 && mp[skill[i]] > 0) {
+                if (mp[skill[i]] > 0) {
 
                     sum += skill[i] * rem;
                     mp[rem]--;
