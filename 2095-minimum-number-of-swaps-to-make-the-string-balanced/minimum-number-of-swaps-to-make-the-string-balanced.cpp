7f3d4989416939de176056a7425f3994411710ea
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-10-08 22:57:03 +0530

    Time: 119 ms (35.68%) | Memory: 35.1 MB (41.42%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -5,14 +5,39 @@ public:
         int inbalance_cnt = 0;
 
         int cb = 0;
+        stack<char> st;
 
         for (int i = 0; i < s.size(); i++) {
             if (s[i] == '[')
-                cb--;
+                st.push(s[i]);
+
+            else if (st.empty())
+
+                inbalance_cnt++;
+
             else
-                cb++;
-            inbalance_cnt = max(cb, inbalance_cnt);
+                st.pop();
         }
         return (inbalance_cnt + 1) / 2;
     }
 };
+
+// ==============Optimal Approach================
+// class Solution {
+// public:
+//     int minSwaps(string s) {
+
+//         int inbalance_cnt = 0;
+
+//         int cb = 0;
+
+//         for (int i = 0; i < s.size(); i++) {
+//             if (s[i] == '[')
+//                 cb--;
+//             else
+//                 cb++;
+//             inbalance_cnt = max(cb, inbalance_cnt);
+//         }
+//         return (inbalance_cnt + 1) / 2;
+//     }
+// };
