72ecc945e90a29632933cb0361bb101aa6dffa79
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-10-08 22:52:09 +0530

    Time: 101 ms (70.15%) | Memory: 32.7 MB (54.72%) - LeetSync

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+
+        int inbalance_cnt = 0;
+
+        int ob = 0, cb = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '[')
+                cb--;
+            else
+                cb++;
+            inbalance_cnt = max(cb, inbalance_cnt);
+        }
+        return (inbalance_cnt + 1) / 2;
+    }
+};
