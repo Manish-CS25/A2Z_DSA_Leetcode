5df7e2c9ec73d7148deee627146ffa963cd92a2b
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-10-22 00:11:11 +0530

    Time: 2 ms (60.79%) | Memory: 10.5 MB (19.97%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,23 +1,60 @@
+// class Solution {
+// public:
+//     void permute(int index, vector<int> nums, vector<vector<int>>& ans) {
+//         if (index == nums.size()) {
+//             ans.push_back(nums);
+//             return;
+//         }
+
+//         for (int i = index; i < nums.size(); i++) {
+//             swap(nums[index], nums[i]);
+//             permute(index + 1, nums, ans);
+//             swap(nums[index], nums[i]);
+//         }
+//     }
+
+//     vector<vector<int>> permute(vector<int>& nums) {
+
+//         vector<vector<int>> ans;
+
+//         permute(0, nums, ans);
+
+//         return ans;
+//     }
+// };
+
+// ================Another Approach===========================
+
 class Solution {
 public:
-    void permute(int index, vector<int> nums, vector<vector<int>>& ans) {
-        if (index == nums.size()) {
-            ans.push_back(nums);
+    void permute(vector<int> nums, vector<int>& ds, int fq[],
+                 vector<vector<int>>& ans) {
+        if (ds.size() == nums.size()) {
+            ans.push_back(ds);
             return;
         }
 
-        for (int i = index; i < nums.size(); i++) {
-            swap(nums[index], nums[i]);
-            permute(index + 1, nums, ans);
-            swap(nums[index], nums[i]);
+        for (int i = 0; i < nums.size(); i++) {
+            if (fq[i] == 0) {
+                ds.push_back(nums[i]);
+                fq[i] = 1;
+                permute(nums, ds, fq, ans);
+
+                fq[i] = 0;
+                ds.pop_back();
+            }
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
 
         vector<vector<int>> ans;
+        vector<int> ds;
+        int fq[nums.size()];
+        for (int i = 0; i < nums.size(); i++)
+            fq[i] = 0;
 
-        permute(0, nums, ans);
+        permute(nums, ds, fq, ans);
 
         return ans;
     }
