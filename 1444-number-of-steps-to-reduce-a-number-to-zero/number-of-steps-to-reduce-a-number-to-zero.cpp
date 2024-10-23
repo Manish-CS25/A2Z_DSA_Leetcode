bcc7b12292e0b559ea4b6d0f203e28d03e0d9f66
Maxx <72138096+Manish-CS25@users.noreply.github.com>
2024-10-23 23:48:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (82.49%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,22 +1,37 @@
+// class Solution {
+// public:
+//     int numberOfSteps(int num) {
+
+//         int s = 0;
+
+//         while (num) {
+//             s++;
+
+//             if (num % 2==0) {
+//                 num /= 2;
+//             }
+
+//             else {
+//                 num -= 1;
+//             }
+
+//         }
+
+//         return s;
+//     }
+// };
+
 class Solution {
 public:
     int numberOfSteps(int num) {
 
-        int s = 0;
+        if (num == 0)
+            return 0;
 
-        while (num) {
-            s++;
+        if (num % 2 == 0)
 
-            if (num % 2==0) {
-                num /= 2;
-            }
-            
-            else {
-                num -= 1;
-            }
-       
-        }
+            return 1 + numberOfSteps(num /= 2);
 
-        return s;
+        return 1 + numberOfSteps(num -= 1);
     }
 };
