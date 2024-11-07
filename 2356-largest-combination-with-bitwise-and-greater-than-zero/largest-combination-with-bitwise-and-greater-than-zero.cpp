bc7cc068581c9f92100d351cda610c79c89c6dcd
Manish Bairwa <72138096+Manish-CS25@users.noreply.github.com>
2024-11-07 19:54:05 +0530

    Time: 35 ms (43.28%) | Memory: 60.1 MB (74.40%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -7,7 +7,7 @@ public:
 
         for (int i = 0; i < 32; i++) {
             int cnt = 0;
-            for (auto candidate : candidates) {
+            for (int &candidate : candidates) {
 
                 // check i-th  bit set bits count for every candidate
                 if (candidate & (1 << i))
