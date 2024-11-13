5485acd77ace61d0a4628a8ab0d0eff59892fa14
Manish Bairwa <72138096+Manish-CS25@users.noreply.github.com>
2024-11-13 20:47:11 +0530

    Time: 40 ms (40.83%) | Memory: 17.3 MB (64.55%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -6,28 +6,73 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+//         ListNode* ans = NULL;
+
+//         unordered_map<ListNode*, int> mp;
+
+//         while (headA) {
+//             mp[headA] = 1;
+//             headA = headA->next;
+//         }
+
+//         while (headB) {
+//             if (mp.find(headB) != mp.end()) {
+//                 return headB;
+//                 break;
+//             }
+//             headB = headB->next;
+//         }
+
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
         ListNode* ans = NULL;
 
-        unordered_map<ListNode*  , int> mp;
+        int size1 = 0, size2 = 0;
 
-        while(headA){
-            mp[headA]=1;
-            headA= headA->next;
+        if (!headA || !headB)
+            return ans;
+        ListNode* tempA = headA;
+        ListNode* tempB = headB;
+        while (tempA) {
+            size1++;
+            tempA = tempA->next;
         }
 
-        while(headB){
-            if(mp.find(headB)!=mp.end()){
-                return headB;
-                break;
+        while (tempB) {
+            size2++;
+            tempB = tempB->next;
+        }
+
+        tempA = headA;
+        tempB = headB;
+
+        if (size1 > size2) {
+            int diff = size1 - size2;
+            while (diff--) {
+                tempA = tempA->next;
             }
-            headB = headB->next;
+        } else if (size2 > size1) {
+            int diff = size2 - size1;
+            while (diff--) {
+                tempB = tempB->next;
+            }
+        }
+
+        while (tempA && tempB) {
+            if (tempA == tempB)
+                return tempA;
+            tempA = tempA->next;
+            tempB = tempB->next;
         }
 
         return ans;
-
-
     }
 };
