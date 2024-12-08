9df61980b5b7fa26927344098a80b2ef3c6c7caf
Manish Bairwa <72138096+Manish-CS25@users.noreply.github.com>
2024-12-08 15:23:12 +0530

    Time: 17 ms (95.11%) | Memory: 61.3 MB (73.84%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,13 +1,28 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         unordered_set<int > SET;
+
+//         for(auto it:nums){
+//             if(SET.find(it)!=SET.end()){
+//                 return true;
+//             }
+//             SET.insert(it);
+//         }
+
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int > SET;
+        sort(nums.begin(), nums.end());
 
-        for(auto it:nums){
-            if(SET.find(it)!=SET.end()){
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
                 return true;
             }
-            SET.insert(it);
         }
 
         return false;
