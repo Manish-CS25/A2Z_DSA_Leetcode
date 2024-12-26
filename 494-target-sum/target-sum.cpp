1b816267209194b40da8fcf453ecbabad751eb84
Manish Bairwa <72138096+Manish-CS25@users.noreply.github.com>
2024-12-27 00:48:13 +0530

    Time: 478 ms (28.67%) | Memory: 11.5 MB (78.96%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -3,11 +3,16 @@ public:
     int res = 0;
     int target;
     int sum = 0;
+    int limits[21] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     void dfs(vector<int>& nums, int pos = 0) {
+        // checking if we finished using all numbers on this branch
         if (pos == nums.size()) {
+            // and in case incrementing res
             if (sum == target) res++;
             return;
         }
+        // dropping the DFS when it makes no sense to continue
+        if (abs(target - sum) > limits[pos]) return;
         // trying with minus first
         sum -= nums[pos];
         dfs(nums, pos + 1);
@@ -18,6 +23,7 @@ public:
         sum -= nums[pos];
     }
     int findTargetSumWays(vector<int>& nums, int s) {
+        for (int i = nums.size() - 1; i >= 0; i--) limits[i] = limits[i + 1] + nums[i];
         target = s;
         dfs(nums);
         return res;
