7d09e99ceaf55ffdd42c366cbc9b574177bc1471
Manish Bairwa <72138096+Manish-CS25@users.noreply.github.com>
2025-01-01 14:24:36 +0530

    Time: 3 ms (18.71%) | Memory: 9.4 MB (11.67%) - LeetSync

---
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        int zeros  =0;
+        int ones = 0;
+        int n = s.size();
+        vector<pair<int, int>> p(n);
+        int i=0;
+        for(auto it:s){ 
+            if(it=='0') zeros++;
+            if(it=='1') ones++;
+
+            p[i++]={zeros , ones};
+            // cout<<ones<<zeros;
+        }
+        int ans = 0;
+        for(int i=0; i<s.size()-1;i++){
+           
+           ans = max(p[i].first + ones-p[i].second , ans);
+        }
+        
+        return ans;
+    }
+};
