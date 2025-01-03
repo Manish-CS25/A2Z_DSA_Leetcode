58c13bcd9afde8023b336296cda873a848f71955
Manish Bairwa <72138096+Manish-CS25@users.noreply.github.com>
2025-01-03 10:55:31 +0530

    Time: 0 ms (100.00%) | Memory: 89.4 MB (62.32%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
@@ -1,23 +1,54 @@
+// class Solution {
+// public:
+//     int waysToSplitArray(vector<int>& nums) {
+
+//         int cnt = 0;
+//         int n = nums.size();
+
+//         vector<long long> prefix(n, 0);
+//         prefix[0] = nums[0];
+//         for (int i = 1; i < n; i++) {
+//             prefix[i] = nums[i] + prefix[i - 1];
+//         }
+
+//         for (int i = 0; i < n - 1; i++) {
+
+//             if (prefix[i] >= (prefix[n - 1] - prefix[i])) {
+//                 cnt++;
+//             }
+//         }
+
+//         return cnt;
+//     }
+// };
+
+
+
+/// More optimal
 class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
+        // Keep track of sum of elements on left and right sides
+        long long leftSum = 0, rightSum = 0;
 
-        int cnt = 0;
-        int n = nums.size();
-
-        vector<long long> prefix(n, 0);
-        prefix[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            prefix[i] = nums[i] + prefix[i - 1];
+        // Initially all elements are on right side
+        for (int num : nums) {
+            rightSum += num;
         }
 
-        for (int i = 0; i < n - 1; i++) {
+        int count = 0;
+        // Try each possible split position
+        for (int i = 0; i < nums.size() - 1; i++) {
+            // Move current element from right to left side
+            leftSum += nums[i];
+            rightSum -= nums[i];
 
-            if (prefix[i] >= (prefix[n - 1] - prefix[i])) {
-                cnt++;
+            // Check if this creates a valid split
+            if (leftSum >= rightSum) {
+                count++;
             }
         }
 
-        return cnt;
+        return count;
     }
 };
