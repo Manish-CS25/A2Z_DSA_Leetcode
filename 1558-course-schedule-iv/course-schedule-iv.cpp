55f5bcda9876cf1b74ca9be424e1205b3d2d7acf
Manish Bairwa <72138096+Manish-CS25@users.noreply.github.com>
2025-01-28 00:07:25 +0530

    Time: 175 ms (19.65%) | Memory: 199.3 MB (5.17%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool dfs(int s , int t , vector<vector<int>> &adj, vector<int> &vis ){
+        if(s==t) return true;
+        vis[s]=1;
+        for(auto it:adj[s]){
+            if(vis[it]!=1 && dfs(it , t , adj , vis)) return true;
+            
+        }
+        return false;
+    }
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        
+        vector<vector<int>> adj(numCourses);
+        vector<bool> ans;
+        int n = prerequisites.size();
+        for(int i=0;i<n;i++){
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        int m = queries.size();
+        for(int i=0;i<m;i++){
+            int s = queries[i][0];
+            int t = queries[i][1];
+            vector<int> vis(numCourses , 0);
+            stack<int> st;
+            st.push(s);
+            if(dfs(s , t , adj , vis)){
+                ans.push_back(1);
+            }
+            else ans.push_back(0);
+        }
+       return ans;
+    }
+};
