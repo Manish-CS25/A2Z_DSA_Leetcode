f9c8bd8b77a7bc45aec497d6192b46ec9d790fc0
Manish Bairwa <72138096+Manish-CS25@users.noreply.github.com>
2025-01-30 22:47:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (99.17%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,18 +1,40 @@
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         int n = x / 2;
+//         int ans = 0;
+//         if(x==1 ) return x;
+
+//         for (long long i = 1; i <= n; i++) {
+//             if (i * i <= x) {
+//                 ans = i;
+//             }
+
+//             else
+//                 break;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int mySqrt(int x) {
-        int n = x / 2;
-        int ans = 0;
-        if(x==1 ) return x;
+        long long low = 0;
+        long long high = x;
 
-        for (long long i = 1; i <= n; i++) {
-            if (i * i <= x) {
-                ans = i;
-            }
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
 
-            else
+            if (mid * mid == x) {
+                return mid;
                 break;
+            } else if (mid * mid < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
-        return ans;
+        return high;
     }
 };
