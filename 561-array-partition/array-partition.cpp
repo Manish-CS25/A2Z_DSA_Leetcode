3f1caed569bc6cf4f404bf1ab8a55804f94fa188
Manish Bairwa <72138096+Manish-CS25@users.noreply.github.com>
2025-02-23 02:20:00 +0530

    Time: 1 ms (92.78%) | Memory: 32 MB (56.00%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -3,16 +3,14 @@ public:
     int arrayPairSum(vector<int>& nums) {
 
         int n = nums.size();
-        sort(nums.begin() , nums.end());
+        sort(nums.begin(), nums.end());
 
-        int sum =0;
-        for(int i=0;i<n;i++){
-            if(i%2==0){
-                sum += nums[i];
-            }
+        int sum = 0;
+        for (int i = 0; i < n; i += 2) {
+
+            sum += nums[i];
         }
 
         return sum;
-        
     }
 };
