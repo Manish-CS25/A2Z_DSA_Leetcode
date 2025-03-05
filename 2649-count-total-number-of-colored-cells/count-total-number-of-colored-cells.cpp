c35b4d0a7b98cbd9bf7205f438d62fcc9a98cbc1
Manish Bairwa <72138096+Manish-CS25@users.noreply.github.com>
2025-03-05 20:09:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (91.36%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        if (n == 1)
-            return 1;
-        if (n == 2)
-            return 5;
+        // if (n == 1)
+        //     return 1;
+        // if (n == 2)
+        //     return 5;
 
-        return coloredCells(n - 1) + 4 + 4*(n - 2);
+        // return coloredCells(n - 1) + 4 + 4*(n - 2);
+
+        return pow(n - 1, 2) + pow(n, 2);
     }
 };
